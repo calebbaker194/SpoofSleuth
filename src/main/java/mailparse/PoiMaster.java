e3cff549966d70e3c00a3481e3d042849b727b88
Unknown <caleb.baker194@gmail.com>
2019-01-15 14:59:34 -0600

    Code Cleanup

--- src/main/java/mailparse/PoiMaster.java
+++ src/main/java/mailparse/PoiMaster.java
@@ -2,17 +2,12 @@ package mailparse;
 
 import java.io.IOException;
 import java.io.InputStream;
-
-import org.apache.poi.poifs.eventfilesystem.POIFSReader;
-import org.apache.poi.poifs.filesystem.FileMagic;
-import org.apache.poi.poifs.macros.VBAMacroExtractor;
 import org.apache.poi.poifs.macros.VBAMacroReader;
 
 
 public class PoiMaster {
 	
 	public static boolean hasMacro(InputStream microFile) {
-
 		
 		try
 		{
